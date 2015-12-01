cb6c0255999f68eb6b348a9843020460d9c7b8e3
Nitin Reddy <redknitin@gmail.com>
2015-12-01 19:22:40 +0400

    Added Console application to run the reference implementation

---
+++ ReferenceConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using ReferenceNotificationImplementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecordObserver rt = new RecordObserver();
+            rt.StartTracking();
+        }
+    }
+}
