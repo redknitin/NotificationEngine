d9784ee7dd8602810899903bdcdf033afb98fe68
Nitin Reddy <redknitin@gmail.com>
2015-12-01 19:22:09 +0400

    Renamed RecordTracker to RecordObserver. Added IObserver implementation.

--- ReferenceNotificationImplementation/RecordObserver.cs
+++ ReferenceNotificationImplementation/RecordObserver.cs
@@ -6,26 +6,42 @@ using System.Threading.Tasks;
 
 namespace ReferenceNotificationImplementation
 {
-    public class RecordTracker
+    public class RecordObserver : NotificationEngine.IObserver<RecordModel>
     {
         private System.Threading.Thread threadTracker = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(TrackMethod));
         RecordSubject rsub = new RecordSubject();
         
         public int timeoutMilli = 3000;
 
-        public void BeginTracking()
+        public void StartTracking()
         {
             threadTracker.Start(this);
         }
 
         private static void TrackMethod(object obj)
         {
-            RecordTracker myObj = (RecordTracker)obj;
+            RecordObserver myObj = (RecordObserver)obj;
+            myObj.rsub.Attach(myObj);
             while (true)
             {
                 myObj.rsub.RefreshState();
                 System.Threading.Thread.Sleep(myObj.timeoutMilli);
             }
+        }
+
+        public void StopTracking()
+        {
+            threadTracker.Abort();
+        }
+
+        public void Update(RecordModel aOld, RecordModel aNew)
+        {
+            Console.WriteLine("Was: " + (aOld == null ? "" : aOld.ID) + " " + (aOld==null?"":aOld.ModifiedDate.ToString()) + "  -->  Is: " + aNew.ID + " " + aNew.ModifiedDate.ToString());
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Detected change");
         }
     }
 }
